e498145a63be38568e1adfc7a01ab85d3a140f0d
Julius Cebreros <me@juliuscebreros.com>
2015-04-29 23:03:09 +1200

    Initial commit

---
+++ src/js/preloader.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  function Preloader() {
+    this.asset = null;
+    this.ready = false;
+  }
+
+  Preloader.prototype = {
+
+    preload: function () {
+      this.asset = this.add.sprite(this.game.width * 0.5 - 110, this.game.height * 0.5 - 10, 'preloader');
+
+      this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+      this.load.setPreloadSprite(this.asset);
+
+      this.loadResources();
+    },
+
+    loadResources: function () {
+      this.load.image('dot', 'assets/dot.png');
+      this.load.image('tiles', 'assets/pacman-tiles.png');
+      this.load.spritesheet('pacman', 'assets/pacman.png', 32, 32);
+      this.load.tilemap('map', 'assets/pacman-small.json', null, Phaser.Tilemap.TILED_JSON);      
+    },
+
+    create: function () {
+      this.asset.cropEnabled = false;
+    },
+
+    update: function () {
+      if (!!this.ready) {
+        this.game.state.start('menu');
+      }
+    },
+
+    onLoadComplete: function () {
+      this.ready = true;
+    }
+  };
+
+  window['pacdungeon'] = window['pacdungeon'] || {};
+  window['pacdungeon'].Preloader = Preloader;
+
+}());
