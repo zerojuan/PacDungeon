b0904de3f8b2df55896b774cfcb6a9452c582232
Julius Cebreros <me@juliuscebreros.com>
2015-08-03 22:54:08 +1200

    Disable babelify for now

--- gulpfile.js
+++ gulpfile.js
@@ -104,7 +104,7 @@ gulp.task('connect', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch([paths.js, paths.js6], ['lint', 'babelify']);
+  gulp.watch([paths.js], ['lint']);
   gulp.watch(['./src/index.html', paths.css, paths.js], ['html']);
 });
 
