62d9b79fbbf9a352702829d9c19cae4e8f816fad
Julius Cebreros <me@juliuscebreros.com>
2015-11-17 22:08:21 +1300

    Add powerup class

---
+++ src/js/Powerup.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  /**
+  * Takes a gameobject and applies effects on it for a defined period
+  */
+  function Powerup(){
+
+  }
+
+  window['pacdungeon'] = window['pacdungeon'] || {};
+  window['pacdungeon'].Powerup = Powerup;
+}());
