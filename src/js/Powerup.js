dfdd213c5d0134f05f0793286bc9dd7acfe4d8ee
Julius Cebreros <me@juliuscebreros.com>
2015-11-18 22:44:34 +1300

    Powerup skeleton

--- src/js/Powerup.js
+++ src/js/Powerup.js
@@ -1,12 +1,28 @@
 (function() {
   'use strict';
 
+  /** ======================================
+  * LIST OF POWERUP EFFECTS
+  *** =====================================*/
+  function freeze(){
+
+  }
+
+
   /**
   * Takes a gameobject and applies effects on it for a defined period
   */
-  function Powerup(){
-
+  function Powerup( type ) {
+    
   }
+
+  Powerup.prototype.setHost = function( host ) {
+    this.host = host;
+  };
+
+  Powerup.prototype.update = function(){
+
+  };
 
   window['pacdungeon'] = window['pacdungeon'] || {};
   window['pacdungeon'].Powerup = Powerup;
